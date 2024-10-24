0659c344871b8064d309a056735f8f4e5b7d177d
Krishna Lokhande <krishnalokhande987@gmail.com>
2024-10-24 16:43:08 +0530

    cors

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
+const bodyParser = require('body-parser');
 
 const app = express();
+const port = 3000;
+
+app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
+// Use the router for handling routes
 app.use(router);
 
-app.get('/', (req, res) => {
-  console.log('GET request received!');
-  res.send('Server is running');
-});
-
-const port = 3000;
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
